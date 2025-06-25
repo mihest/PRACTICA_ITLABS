e45ff95c5755f7b68effc92975de540327cf27f0
ligets <legen2a208@gmail.com>
2025-06-25 12:20:59 +0400

    Header is ready

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
 
 
-const Header = () => {
+const Header = ({className}) => {
     return(
-        <header>
-
+        <header className={`w-full h-[164px] flex items-center ${className}`}>
+            <img src="/logo.png" alt="logo"/>
+            <div className="flex flex-col ms-[40px]">
+                <span className="text-[64px]/[100%] text-[#2B2A29] font-bold">Музей Боевой и Трудовой Славы</span>
+                <span className="text-[58px]/[100%] text-[#514F4D] font-bold">город Александров</span>
+            </div>
         </header>
     )
 }
--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -3,10 +3,10 @@ import Header from "../components/Header.jsx";
 
 const MainLayout = ({ children }) => {
     return (
-        <div className="flex flex-col h-[1080px] bg-[#F2E5CC] relative">
-            <div className="w-full h-[1080px] bg-[url('/background.png')] bg-center bg-no-repeat opacity-40 absolute top-0 left-0"></div>
-            <Header />
-            <main className="flex flex-1">
+        <div className="flex flex-col h-[1080px] bg-[#F2E5CC] relative p-[80px] pe-[0px]">
+            <div className="w-full h-[1080px] bg-[url('/background.png')] bg-center bg-no-repeat opacity-40 absolute top-0 left-0 z-1"></div>
+            <Header className="z-2" />
+            <main className="flex flex-1 z-2">
                 {children || <Outlet />}
             </main>
         </div>
